8033b923af8f6240ae818cd4c6021342886ed660
Manfred Stienstra <manfred@fngtps.com>
2010-03-27 11:44:42 +0100

    Add documentation.

--- lib/validation_sets.rb
+++ lib/validation_sets.rb
@@ -1,8 +1,11 @@
 module ValidationSets
   autoload :ValidationSet, 'validation_sets/validation_set'
   
+  # Used to set the current validation set during definition of the model
   attr_accessor :current_validation_set
   
+  # Returns the name of the validation callback method for a save method and the label of a
+  # validation set.
   def validation_set_method(on, label)
     case on
       when :save   then "validate_#{label}_set".to_sym
@@ -11,6 +14,7 @@ module ValidationSets
     end
   end
   
+  # Returns the name of the validation callback method for a save method.
   def validation_method(on)
     if current_validation_set
       validation_set_method(on, current_validation_set)
@@ -19,6 +23,13 @@ module ValidationSets
     end
   end
   
+  # Add a validation set to the model.
+  #
+  #   class Account < ActiveRecord::Base
+  #     validation_set_for(:activation) do |set|
+  #       set.validates_presence_of :username
+  #     end
+  #   end
   def validation_set_for(label, &block)
     [:save, :create, :update].each do |save_method|
       callback_chain = validation_method(save_method)
@@ -39,15 +50,27 @@ module ValidationSets
   end
   
   module InstanceMethods
+    # Note that you want to protect this attribute to make sure people can't change the validation
+    # set from the form
     attr_reader :_validation_set
     
+    # Set the active validation set on the model. After calling this both the global as well as
+    # the validations defined in the set will run.
+    #
+    # Example:
+    #
+    #   account = Account.new
+    #   account.use_validation_set(:admin) # Turns on the :admin validation set
+    #   account.use_validation_set(nil)    # Turns off all validation sets
     def use_validation_set(set)
       @_validation_set = set
     end
   end
 end
 
-class ActiveRecord::Base
-  extend ValidationSets
-  include ValidationSets::InstanceMethods
+module ActiveRecord #:nodoc:
+  class Base #:nodoc:
+    extend ValidationSets
+    include ValidationSets::InstanceMethods
+  end
 end
--- lib/validation_sets/validation_set.rb
+++ lib/validation_sets/validation_set.rb
@@ -1,22 +1,28 @@
 module ValidationSets
+  # A ValidationSet instance is used to redirect the original validation methods (validate,
+  # validate_on_create, and validate_on_update) to the callback chain for the set.
   class ValidationSet
     def initialize(model, label)
       @model = model
       @label = label
     end
     
+    # Adds a validation method or proc that always runs
     def validate(*params, &block)
       send(validation_set_method(:save, @label), *params, &block)
     end
     
+    # Adds a validation method or proc that runs on create
     def validate_on_create(*params, &block)
       send(validation_set_method(:create, @label), *params, &block)
     end
     
+    # Adds a validation method or proc that runs on update
     def validate_on_update(*params, &block)
       send(validation_set_method(:update, @label), *params, &block)
     end
     
+    # Forwards all other methods (ie. validates_presence_of) to the model class.
     def method_missing(method, *attributes, &block)
       @model.send(method, *attributes, &block)
     end
